18fd44041f02ea718018e00f8323c919d7c30cb5
Md Rasedul Islam <rased100@gmail.com>
2022-03-13 03:58:05 +0600

    market

--- src/pages/MarketStatus/MarketStatus.js
+++ src/pages/MarketStatus/MarketStatus.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './MarketStatus.css';
 
 const MarketStatus = () => {
     return (
-        <div>
-            MarketStatus
+        <div className='marketContainer'>
+            <h2>In the past 24 hours <span className='mdColor'>Market is down</span> <span className='pColor'>1.14%</span></h2>
         </div>
     );
 };
