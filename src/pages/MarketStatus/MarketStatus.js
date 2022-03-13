0c023d90d78a9912703c98412084eced58afc09e
Md Rasedul Islam <rased100@gmail.com>
2022-03-13 23:16:22 +0600

    market

--- src/pages/MarketStatus/MarketStatus.js
+++ src/pages/MarketStatus/MarketStatus.js
@@ -3,8 +3,10 @@ import './MarketStatus.css';
 
 const MarketStatus = () => {
     return (
-        <div className='marketContainer'>
-            <h2>In the past 24 hours <span className='mdColor'>Market is down</span> <span className='pColor'>1.14%</span></h2>
+        <div className='d-flex justify-content-center'>
+            <div className='marketContainer'>
+                <h2>In the past 24 hours <span className='mdColor'>Market is down</span> <span className='pColor'>1.14%</span></h2>
+            </div>
         </div>
     );
 };
