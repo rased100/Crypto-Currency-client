dcdd5144d272f9cc763f50d3ee11bbf4d99b8e73
Md Rasedul Islam <rased100@gmail.com>
2022-03-14 00:23:51 +0600

    top gainer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from './pages/Footer/Footer';
 import Header from './pages/Header/Header';
 import MarketStatus from './pages/MarketStatus/MarketStatus';
 import Register from './pages/Register/Register';
+import Topgainer from './pages/Topgainer/Topgainer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <MarketStatus></MarketStatus>
+      <Topgainer></Topgainer>
       <Register></Register>
       <Footer></Footer>
     </div>
