fee30e51730462e8082fc5b73193261b48e5566b
Md Rasedul Islam <rased100@gmail.com>
2022-03-13 22:54:08 +0600

    footer done

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Footer.css';
+import cryptupLogo from '../../data/image/fbg.png';
 
 const Footer = () => {
     return (
         <div className='fContainer'>
-            <div className='fTopSection'>
-
+            <div className='d-flex justify-content-around align-items-center fTopSection'>
+                <div className='footerIcon'>
+                    <img src={cryptupLogo} alt="" />
+                </div>
+                <div className='d-flex justify-content-around aboutUs'>
+                    <div className='aboutText'>
+                        <h4>About Us</h4>
+                        <h4>Portfolio</h4>
+                        <h4>Contact Us</h4>
+                    </div>
+                    <div className='buysellText'>
+                        <h4>Buy & sell</h4>
+                        <h4>Wallet</h4>
+                        <h4>Card</h4>
+                    </div>
+                </div>
+                <div className='footerContuctUs'>
+                    <h1>Contact Us</h1>
+                </div>
             </div>
             <div className='fBottomSection'>
                 <div className='d-flex justify-content-evenly justify-content-center fEnd'>
