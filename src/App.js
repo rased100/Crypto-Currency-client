b74c53857bd8ebd8dfe2939fe13cb9048bfa8c5b
Md Rasedul Islam <rased100@gmail.com>
2022-03-13 12:47:16 +0600

    footer end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Footer from './pages/Footer/Footer';
 import Header from './pages/Header/Header';
 import MarketStatus from './pages/MarketStatus/MarketStatus';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <MarketStatus></MarketStatus>
+      <Register></Register>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='fContainer'>
+            <div className='fTopSection'>
+
+            </div>
+            <div className='fBottomSection'>
+                <div className='d-flex justify-content-evenly justify-content-center fEnd'>
+                    <h4>© Jessan  - All Rights Reserved</h4>
+                    <h4> About | Privacy Policy | Term & Service</h4>
+                    <h4><span>Web Design </span>JK</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
